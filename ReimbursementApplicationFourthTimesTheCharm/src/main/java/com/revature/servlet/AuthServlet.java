ddb5d93c02901f6969a4a05591b4f91881f27a06
unknown <sunbor@oregonstate.edu>
2019-11-11 14:34:20 -0500

    very narrow vertical slice, shows usernames on webpage

---
+++ ReimbursementApplicationFourthTimesTheCharm/src/main/java/com/revature/servlet/AuthServlet.java
@@ -0,0 +1,60 @@
+package com.revature.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.revature.dao.UserDao;
+import com.revature.model.User;
+
+public class AuthServlet extends HttpServlet {
+	
+	private UserDao userDao = UserDao.currentImplementation;
+	
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println(req.getRequestURL());
+		resp.addHeader("Access-Control-Allow-Origin", "http://localhost:5500");
+		resp.addHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, HEAD");
+		resp.addHeader("Access-Control-Allow-Headers",
+				"Origin, Methods, Credentials, X-Requested-With, Content-Type, Accept");
+		resp.addHeader("Access-Control-Allow-Credentials", "true");
+		resp.setContentType("application/json");
+		// TODO Auto-generated method stub
+		super.service(req, resp);
+
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("uri = " + req.getRequestURI());
+		if ("/ReimbursementApplicationFourthTimesTheCharm/auth/login".equals(req.getRequestURI())) {
+			ObjectMapper om = new ObjectMapper();
+			User credentials = (User) om.readValue(req.getReader(), User.class);
+			User loggedInUser = userDao.findByUsernameAndPassword(credentials.getUsername(), credentials.getPassword());
+			if (loggedInUser == null) {
+				resp.setStatus(401); // Unauthorized status code
+				return;
+			} else {
+				resp.setStatus(201);
+				req.getSession().setAttribute("user", loggedInUser);
+				resp.getWriter().write(om.writeValueAsString(loggedInUser));
+				return;
+			}
+		}
+	}
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		if ("/ReimbursementApplicationFourthTimesTheCharm/auth/session-user".equals(req.getRequestURI())) {
+			ObjectMapper om = new ObjectMapper();
+			String json = om.writeValueAsString(req.getSession().getAttribute("user"));
+			resp.getWriter().write(json);
+		}
+	}
+	
+}
--- ReimbursementApplicationFourthTimesTheCharm/src/main/java/com/revature/servlet/UserServlet.java
+++ ReimbursementApplicationFourthTimesTheCharm/src/main/java/com/revature/servlet/UserServlet.java
@@ -17,6 +17,19 @@ public class UserServlet extends HttpServlet {
 	private UserDao userDao = UserDao.currentImplementation;
 	
 	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.service(req, resp);
+		resp.addHeader("Access-Control-Allow-Origin", "http://localhost:5500");
+		resp.addHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, HEAD");
+		resp.addHeader("Access-Control-Allow-Headers",
+				"Origin, Methods, Credentials, X-Requested-With, Content-Type, Accept");
+		resp.addHeader("Access-Control-Allow-Credentials", "true");
+		resp.setContentType("application/json");
+	}
+
+	
+	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		
 		List<User> userList;
